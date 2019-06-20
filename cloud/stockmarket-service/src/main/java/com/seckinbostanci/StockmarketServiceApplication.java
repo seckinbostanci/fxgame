d71d4996d092847a4e8003f72817b959860ddc2f
seckinbostanci <seckinbostanci@gmail.com>
2019-06-20 19:33:58 +0300

    jdde from pretty-tools.com dde library added external maven dependency with dll. this library require for MetaTrader4 dde communication

---
+++ cloud/stockmarket-service/src/main/java/com/seckinbostanci/StockmarketServiceApplication.java
@@ -0,0 +1,11 @@
+package com.seckinbostanci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmarketServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockmarketServiceApplication.class,args);
+    }
+}
